834117be4797e2b703d4745bf05dcf3eea667496
SergWebAcceptance <serg.web@acceptance.ltd>
2026-03-04 19:03:59 +0200

    Update default locale in routing configuration to Bulgarian
    - Changed the default locale from English to Bulgarian in the routing configuration to better align with user preferences and localization strategy.

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -5,5 +5,5 @@ export const routing = defineRouting({
   locales: ['en', 'de', 'it', 'bg'],
   localePrefix: 'as-needed',
   // Used when no locale matches
-  defaultLocale: 'en',
+  defaultLocale: 'bg',
 });
